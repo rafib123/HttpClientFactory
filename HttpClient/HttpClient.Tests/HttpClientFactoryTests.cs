129a95124014deb0ee2a9391e2af547ca85bc042
iamrafi@gmail.com <iamrafi@gmail.com>
2016-06-09 20:57:56 +0100

    htttp client returning a http client

---
+++ HttpClient/HttpClient.Tests/HttpClientFactoryTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpClient;
+
+namespace HttpClient.Tests
+{
+    [TestFixture]
+    public class HttpClientFactoryTests
+    {
+        [Test]
+        public void GetHttpClient_NoInputs_Returns_A_SystemNetHttpClient()
+        {
+            var factory = new HttpClientFactory();
+            var client = factory.GetHttpClient();
+
+            Assert.AreEqual(typeof(System.Net.Http.HttpClient), client.GetType())
+;
+        }
+
+        [Test]
+        public void GetHttpClient_NoInputs_Returns_TheSameInstance_SystemNetHttpClient()
+        {
+            var factory = new HttpClientFactory();
+
+            var client1 = factory.GetHttpClient();
+            var client1Hash = client1.GetHashCode();
+
+            var client2 = factory.GetHttpClient();
+            var client2Hash = client2.GetHashCode();
+
+            Assert.AreEqual(client1Hash, client2Hash);
+        }
+
+        [Test]
+        public void GetHttpClient_BaseUri_Returns_SystemNetHttpClientWithBaseAddress()
+        {
+            var testUri = "http://localhost/api/";
+            var factory = new HttpClientFactory();
+
+            var client = factory.GetHttpClient(testUri);
+
+            Assert.AreEqual(client.BaseAddress, new Uri(testUri));
+        }
+    }
+}
---
+++ HttpClient/HttpClient/HttpClientFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace HttpClient
+{
+    using System.Net.Http;
+    public class HttpClientFactory
+    {
+        private HttpClient _httpClient;
+
+        public HttpClientFactory()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public HttpClient GetHttpClient()
+        {
+            return _httpClient;
+        }
+
+        public HttpClient GetHttpClient(string baseAddress)
+        {
+            _httpClient.BaseAddress = new Uri(baseAddress);
+
+            return _httpClient;
+        }
+    }
+}
